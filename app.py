cc659db5888aaba0bacfab7278814d8edce1b345
providedtroubleshoot <mehmetnazim998@gmail.com>
2025-10-23 15:12:12 +0300

    dockerfile

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ app = Flask(__name__)
 # Selenium WebDriver kurulumu
 def init_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Başsız modda çalıştır
+    chrome_options.binary_location = "/usr/bin/chromium"
+    chrome_options.add_argument("--headless=new")  # Başsız modda çalıştır
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
